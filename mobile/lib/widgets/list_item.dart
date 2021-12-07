2e112815560175e72db33144555d255a0217aca5
darkowl91 <darkowl91@gmail.com>
2021-12-07 16:23:05 +0300

    localize

--- mobile/lib/widgets/list_item.dart
+++ mobile/lib/widgets/list_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:intl/intl.dart';
 import 'package:mobile/model/measurement.dart';
 import 'package:mobile/service/measurement_store.dart';
@@ -26,14 +27,17 @@ class ListItem extends StatelessWidget {
           ),
         ),
       ),
-      title: const Text('Hypertension'),
+      title: I18nText('pressure.hypertension'),
       isThreeLine: true,
       subtitle: Expanded(
         child: Row(
           children: [
             Text(DateFormat.yMEd().format(item.createdAt.toLocal())),
             const Spacer(),
-            Text("${item.pulse.bpm} BPM"),
+            I18nText(
+              'pulse.unit',
+              translationParams: {'bpm': item.pulse.bpm.toString()},
+            ),
             const Spacer()
           ],
         ),
@@ -41,12 +45,12 @@ class ListItem extends StatelessWidget {
       // trailing: const Icon(Icons.more_horiz),
       trailing: PopupMenuButton(
           icon: const Icon(Icons.more_horiz),
-          tooltip: "Edit Measurement",
+          tooltip: FlutterI18n.translate(context, "measurement.edit"),
           itemBuilder: (context) => [
                 PopupMenuItem(
-                  child: const ListTile(
-                      leading: Icon(Icons.delete_forever),
-                      title: Text("Remove")),
+                  child: ListTile(
+                      leading: const Icon(Icons.delete_forever),
+                      title: I18nText('measurement.delete')),
                   onTap: () =>
                       Provider.of<MeasurementStore>(context, listen: false)
                           .remove(item),
